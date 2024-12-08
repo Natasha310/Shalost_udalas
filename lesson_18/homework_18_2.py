d8595c7cc79e569b7bf183a0371080038fb887a2
Natasha310 <sergeevnan123@gmail.com>
2024-12-08 17:47:51 +0300

    hw18_2

--- lesson_18/homework_18_2.py
+++ lesson_18/homework_18_2.py
@@ -1,21 +1,42 @@
 """Ітератори:
-Напишіть ітератор, який повертає всі парні числа в діапазоні від 0 до N."""
-N = 50
+1.Напишіть ітератор, який повертає всі парні числа в діапазоні від 0 до N.
+2.Реалізуйте ітератор для зворотного виведення елементів списку."""
+
+class Iterator:
+    def __init__(self, N):
+        self.N = N
+
+    def __iter__(self):
+        for i in range(self.N):
+           if i%2 <= 0:
+            yield i
 
 
-def iter_even(N):
-    for i in range(N):
-       if i%2 == 0:
-        print(i)
 
-print(iter_even(N))
+iterat1 = Iterator(50)
+for el in iterat1:
+    print(el)
 
-"""Реалізуйте ітератор для зворотного виведення елементів списку."""
 
-list1 = [1,2,3]
-def reversed_iter(list):
-     for i in reversed(list):
-         print(i)
+class Reverse:
+    def __init__(self, list1):
+        self.list1 = list1
 
-print(reversed_iter(list1))
+    def __iter__(self):
+         for itm in reversed(self.list1):
+             yield itm
 
+    def __next__(self):
+        while True:
+            item = next(self.list1, "end")
+            if item == "end":
+                break
+            print(item)
+
+
+
+
+
+iterat2 = Reverse([1,2,3])
+for r in iterat2:
+     print(r)
